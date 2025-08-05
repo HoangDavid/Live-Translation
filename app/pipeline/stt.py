fac4116a3aa08f11bcfa8a57abb77226a8ec361c
HoangDavid <hoangnguyenxuan2004@gmail.com>
2025-08-05 23:06:02 +0700

    add diffing algo for flushing and corrections logic

--- app/pipeline/stt.py
+++ app/pipeline/stt.py
@@ -1,11 +1,11 @@
 import sounddevice as sd
 import numpy as np
-import os
-import functools
-import asyncio, concurrent.futures
+import os,functools, sys, asyncio, concurrent.futures
+from nanoid import generate
 from faster_whisper import WhisperModel, BatchedInferencePipeline
 from dataclasses import dataclass
 from difflib import SequenceMatcher
+import webrtcvad
 
 @dataclass
 class Worker:
@@ -27,7 +27,11 @@ class STT:
 
         # audio sampling config
         self.sample_rate = sample_rate
-        self.block_samples = self.sample_rate // 1000 # process 16 chunk of 1 ms audios
+        self.block_samples = self.sample_rate * 10 // 1000 # process 16 chunk of 1 ms audios
+
+        # silence detection
+        self.vad = webrtcvad.Vad(3)
+        self.silence_tol = 0.8
 
         # stt model thread pooling for heavier tasks
         self._pool = concurrent.futures.ThreadPoolExecutor(max_workers=pool_size)
@@ -38,7 +42,7 @@ class STT:
 
 
     async def new_stt_task(self, language) -> Worker:
-        # spawn a background stt worker
+        # spawn a background stt worker task
         loop = asyncio.get_running_loop()
         stop_evt = asyncio.Event()
         inboundQ = asyncio.Queue()
@@ -52,57 +56,143 @@ class STT:
     async def _worker(self, language: str, inbound: asyncio.Queue, outbound: asyncio.Queue, stop: asyncio.Event) -> ValueError:
         buf = np.zeros(0, dtype=np.float32)
         last_ts = asyncio.get_event_loop().time()
-        prev_words = []
-
+        prev_tokens = []
+        silence = 0
         
         try:
             while not stop.is_set():
                 try:
                     # get and add audio chunk to buffering
                     chunk = await asyncio.wait_for(inbound.get(), timeout=self.transcribe_rate)
-                    
-                except asyncio.TimeoutError:
-                    await asyncio.sleep(0.0)
-                    continue
+                    if await self._is_silence(chunk):
+                        # TODO: add silence to process less chunks
+                        await asyncio.sleep(0)
+                        continue
 
+                except asyncio.TimeoutError:
+                    # yeild
+                    await asyncio.sleep(0)
+                    continue
+                
+                # trim window when reach limit
+                if (buf.shape[0] / self.sample_rate) >= self.context_limit or silence >= self.silence_tol:
+                    buf = buf[int(self.sample_rate * 1): ]
+                    # TODO: reset prev_tokens correctly
 
                 buf = np.concatenate((buf, chunk))
-                if (buf.shape[0] / self.sample_rate) >= self.context_limit:
-                    # reduce the window by 2 second
-                    buf = buf[int(self.sample_rate * 1):]
-
-
+                
+                # call transcribe at fixed rate
                 now = asyncio.get_event_loop().time()
                 if now - last_ts >= self.transcribe_rate:
-                    cur_words = await self._transcribe(prev_words, language, buf)
-                    if cur_words:
-                        pass
+                    prev_tokens, ops = await self._transcribe(prev_tokens, language, buf)
+                    # TODO: add ops to outbound buffer
                 
-                    # yeilding to other coroutines
                     last_ts = now
                 
+                # yeild
                 await asyncio.sleep(0)
 
         except Exception as e:
             print("Failed: ", e)
+            sys.exit()
 
+    async def _is_silence(self, chunk) -> bool:
+        pcm16 = (chunk * 32767).astype(np.int16).tobytes()
+        is_speech = self.vad.is_speech(pcm16, sample_rate=self.sample_rate)
 
-    async def _transcribe(self, prev, language, buf) -> list[str]:
+        return is_speech
+
+    async def _transcribe(self, prev_tokens, language, buf) -> list[str]:
         try:
-            func = functools.partial(
+            # run through stt model
+            segments, _ = await asyncio.get_running_loop().run_in_executor(
+                self._pool,
+                functools.partial(
                 self._pipe.transcribe, buf,
-                language=language, beam_size=10,
-                batch_size=8, vad_filter=True,
+                language=language, beam_size=8,
+                batch_size=16, vad_filter=True,
             )
-
-            segs, _ = await asyncio.get_running_loop().run_in_executor(self._pool, func)
-
-            raw = ' '.join(seg.text for seg in segs).strip()
-            tokens = raw.split()
+)
+            
+            new_tokens = []
+            for segment in segments:
+                if segment.text:
+                    for w in segment.text.strip().split():
+                        new_id = generate("0123456789abcdef", 8)
+                        new_tokens.append((new_id, w))
+            
+            # run diffing algo
+            updated_prev, ops = await asyncio.get_running_loop().run_in_executor(
+                self._pool, functools.partial(
+                self._get_ops, prev_tokens, new_tokens
+            ))
                 
-            return tokens
+            return updated_prev, ops
         except Exception as e:
             print("Transcription faield: ", e)
+            sys.exit()
+
+
+    # corrections and flush new tokens
+    def _get_ops(self, prev_tokens, new_tokens):
+        try:
+            ops = {"flush": [], "delete": [], "insert": [], "replace": []}
+            updated_prev = []
+
+            sm = SequenceMatcher(a=[n for (_, n) in prev_tokens], 
+                                 b=[n for (_, n) in new_tokens])
+
+            # empty sequence
+            if not new_tokens:
+                return updated_prev, ops
+            
+            # finding corrections based on previous inference
+            for tag, i1, i2, j1, j2 in sm.get_opcodes():
+                if tag == "equal":
+                    updated_prev.extend(new_tokens[j1:j2])
+
+                elif tag == "replace":
+                    prev_seq = prev_tokens[i1:i2]
+                    new_seq = new_tokens[j1:j2]
+
+                    overlap = min(len(prev_seq), len(new_seq))
+                    ops["replace"].extend([(prev_seq[k], new_seq[k]) for k in range(overlap)])
+
+                    if len(new_seq) > len(prev_seq):
+                        ops["flush"].extend([new_seq[k] for k in range(overlap, len(new_seq))])
+
+                    elif len(new_seq) < len(prev_seq):
+                        ops["delete"].extend([prev_seq[k] for k in range(overlap, len(prev_seq))])
+
+                    updated_prev.extend(new_seq[j1:j2])
+
+                elif tag == "insert":
+                    if prev_tokens != []:
+                        a_idx = None
+                        b_idx = None
+
+                        if i1 - 1 >= 0:
+                            a_idx, _ = prev_tokens[i1 - 1]
+                        if i1 + 1 < len(prev_tokens):
+                            b_idx, _ = prev_tokens[i1 + 1]
+                                                
+                        ops["insert"].append((new_tokens[j1:j2][0], (a_idx, b_idx)))
+
+                    else:
+                        ops["flush"].extend((new_tokens[j1:j2]))
+
+                    updated_prev.extend(new_tokens[j1:j2])
+
+                elif tag == "delete":
+                    ops["delete"].extend(prev_tokens[i1:i2])
+
+
+            return updated_prev, ops
+
+        except Exception as e:
+            print("Failed to get ops: ", e)
+            sys.exit()
+
         
 
 async def test():
